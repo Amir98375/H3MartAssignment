e1bdd81068ff28679c342971cb9959f26327c49f
Mohd Amir <Amirkhan98375@gmail.com>
2022-09-12 16:12:18 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Navbar } from './Components/Navbar';
 import { ShowPage } from './Components/ShowPage';
 import logo from './logo.svg';
 
@@ -5,6 +6,7 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
           <ShowPage/>
     </div>
   );
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ShowPage.css'
+export const Navbar = () => {
+  return (
+    <div >
+       <div className='main'>
+        <div className='axis'>Crypto sale</div>
+        <div className='axis'> Crypto benifits</div>
+        <div className='axis'>Crypto Buy</div>
+       </div>
+    </div>
+  )
+}
